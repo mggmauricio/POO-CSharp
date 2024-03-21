484ef1a542e102b1627b8cb06311f0efd0004e46
Mauricio Godoy <mauriciomgodoy01@gmail.com>
2024-03-21 15:13:16 -0300

    Aula1 terminada

--- Aula1/MeuPrimeiroProjeto/Program.cs
+++ Aula1/MeuPrimeiroProjeto/Program.cs
@@ -7,7 +7,15 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("3");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+            Console.WriteLine("6");
+            Console.WriteLine("7");
+            Console.WriteLine("8");
+            
         }
     }
 }

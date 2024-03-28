7349de77a36ca64d7cf45aac36d91590aea56722
Mauricio Godoy <mauriciomgodoy01@gmail.com>
2024-03-28 14:05:20 -0300

    Segunda aula sendo adicionada

---
+++ Aula2/First/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Operadores Aritméticos");
+
+            int n1 = 3 + 4 * 2;
+            int n2 = (3+4) * 2;
+            int n3 = 17 / 3;
+            int n4 = 17 % 3;
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            double n5 = Math.Pow(2, 3);
+            Console.WriteLine(n5);
+            n5 += 10;
+            Console.WriteLine(n5);
+            n5 -= 10;
+            Console.WriteLine(n5);
+            n5 *= 10;
+            Console.WriteLine(n5);
+            n5 /= 10;
+            Console.WriteLine(n5);
+            n5 %= 10;
+        }
+    }
+}
